d9350151040ce2ea4d52d36b29f03dd613e74b05
avi-codesmith <avi.codesmith@gmail.com>
2025-05-21 09:15:38 +0530

    Add New Cursor

--- script.js
+++ script.js
@@ -22,26 +22,5 @@ const removeLoader = () => {
   }, 1000);
 };
 
-document.addEventListener("mousemove", (e) => {
-  const trail = document.createElement("div");
-  const randomText = Math.floor(Math.random() * 1);
-  if (randomText === 0) {
-    trail.innerText = "⚫";
-  } else if (randomText === 1) {
-    trail.innerText = "⚪️";
-    trail.innerText = "🟠";
-  } else if (randomText === 2) {
-  }
-  trail.className = "trail";
-  document.body.appendChild(trail);
-
-  trail.style.left = `${e.pageX}px`;
-  trail.style.top = `${e.pageY}px`;
-
-  setTimeout(() => {
-    trail.remove();
-  }, 500);
-});
-
 backgroundChanger();
 removeLoader();
