e631ae1aa4ce7e7784ca96b42469cb3e0ef4c883
avi-codesmith <avi.codesmith@gmail.com>
2025-05-21 08:25:03 +0530

    Add Pointer effect

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const main = document.querySelector("main");
 const loader = document.querySelector(".loader");
 const random = Math.floor(Math.random() * 5);
 const typed = new Typed(".type-writter", {
-  strings: ["Developer", "New Bie", "Programmer", "Student"],
+  strings: ["Developer", "New Bie", "Programmer", "Student", "Codesmith"],
   typeSpeed: 80,
   backSpeed: 50,
   loop: true,
@@ -21,6 +21,27 @@ const removeLoader = () => {
     loader.style.opacity = "0";
   }, 1000);
 };
+
+document.addEventListener("mousemove", (e) => {
+  const trail = document.createElement("div");
+  const randomText = Math.floor(Math.random() * 1);
+  if (randomText === 0) {
+    trail.innerText = "⚫";
+  } else if (randomText === 1) {
+    trail.innerText = "⚪️";
+    trail.innerText = "🟠";
+  } else if (randomText === 2) {
+  }
+  trail.className = "trail";
+  document.body.appendChild(trail);
+
+  trail.style.left = `${e.pageX}px`;
+  trail.style.top = `${e.pageY}px`;
+
+  setTimeout(() => {
+    trail.remove();
+  }, 500);
+});
 
 backgroundChanger();
 removeLoader();
